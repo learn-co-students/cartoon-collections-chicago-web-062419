3e998e8a54708fa6e2620804ea6d9c9af3468ac4
Adrian Popan <adrian.popan@gmail.com>
2019-06-13 22:18:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,22 +14,6 @@ def long_planeteer_calls(array)
   array.any? {|call| call.length > 4}
 end
 
-#   describe "#find_the_cheese" do
-#     it "returns the first element of the array that is cheese" do
-#       contains_cheddar = ["banana", "cheddar", "sock"]
-#       expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-#       contains_gouda = ["potato", "gouda", "camembert"]
-#       expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-#     end
-
-#     it "returns nil if the array does not contain a type of cheese" do
-#       no_cheese = ["ham", "cellphone", "computer"]
-#       expect(find_the_cheese(no_cheese)).to eq nil
-#     end
-#   end
-# end
-
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |maybe_cheese|
